8656bc9b3cd08a845865c9a6daaf69e8e589ee09
IceeCodee <86699334+IceeCodee@users.noreply.github.com>
2022-12-11 22:52:14 -0500

    Create securityBreach.sql

---
+++ Would you LIKE the Second Meal/securityBreach.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT first_name, second_name , attribute
+	FROM users
+	WHERE attribute LIKE BINARY CONCAT('%_\%%',first_name,'\_',second_name, '%\%%')
+ ;
+ /* binary operation compares character by character */
+END
