81ebee164f5b51139ba204f276c6278b547eb929
IceeCodee <86699334+IceeCodee@users.noreply.github.com>
2022-12-13 21:50:51 -0500

    Create travelDiary.sql

---
+++ Would you LIKE the Second Meal/travelDiary.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+SELECT GROUP_CONCAT(DISTINCT country SEPARATOR ';') AS countries 
+    FROM diary
+	GROUP BY 'all'
+	ORDER BY countries ASC;
+
+END
